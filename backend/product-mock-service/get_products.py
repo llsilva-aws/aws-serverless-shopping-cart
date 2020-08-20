7b9015945e11115cf1fe4b18223749f5644b4169
Leonardo Silva <llsilva@amazon.com>
2020-08-20 15:59:48 +0000

    Move code to the right place.

--- backend/product-mock-service/get_products.py
+++ backend/product-mock-service/get_products.py
@@ -27,24 +27,11 @@ table = dynamodb.Table("products")
 """with open('product_list.json', 'r') as product_list:
     product_list = json.load(product_list)
 """
-logger.info("Fetching Products from DB")
-response = table.scan()
-items = response['Items']
-
-logger.info("Products in the table: " + str(len(response['Items'])))
-
-while 'LastEvaluatedKey' in response:
-    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    items.extend(response['Items'])
-    
-product_list = json.dumps(items, cls=DecimalEncoder)
-
 HEADERS = {
     "Access-Control-Allow-Origin": os.environ.get("ALLOWED_ORIGIN"),
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
 }
-
 
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler
@@ -52,7 +39,17 @@ def lambda_handler(event, context):
     """
     Return list of all products.
     """
-    logger.debug("Fetching product list")
+    logger.info("Fetching Products from DB")
+    response = table.scan()
+    items = response['Items']
+
+    logger.info("Products in the table: " + str(len(response['Items'])))
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        items.extend(response['Items'])
+    
+    product_list = json.dumps(items, cls=DecimalEncoder)
 
     return {
         "statusCode": 200,
